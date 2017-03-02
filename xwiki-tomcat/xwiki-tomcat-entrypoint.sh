0f6fdd790b7689c1caba6ce08ba95bd004baccf2
Denis GERMAIN <dt.germain@gmail.com>
2017-03-02 13:33:16 +0100

    Big change in URL !!
    By default, URL was http://<IP>:8080/xwiki, now htpp://<IP>:8080/
    (trailing xwiki removed)

--- xwiki-tomcat/xwiki-tomcat-entrypoint.sh
+++ xwiki-tomcat/xwiki-tomcat-entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 cd /usr/local/tomcat
 
-sed -i "s/POSTGRES_INSTANCE/${POSTGRES_INSTANCE}/g" webapps/xwiki/WEB-INF/hibernate.cfg.xml
-sed -i "s/POSTGRES_PORT/${POSTGRES_PORT}/g" webapps/xwiki/WEB-INF/hibernate.cfg.xml
-sed -i "s/POSTGRES_DB/${POSTGRES_DB}/g" webapps/xwiki/WEB-INF/hibernate.cfg.xml
-sed -i "s/POSTGRES_USER/${POSTGRES_USER}/g" webapps/xwiki/WEB-INF/hibernate.cfg.xml
-sed -i "s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g" webapps/xwiki/WEB-INF/hibernate.cfg.xml
+sed -i "s/POSTGRES_INSTANCE/${POSTGRES_INSTANCE}/g" webapps/ROOT/WEB-INF/hibernate.cfg.xml
+sed -i "s/POSTGRES_PORT/${POSTGRES_PORT}/g" webapps/ROOT/WEB-INF/hibernate.cfg.xml
+sed -i "s/POSTGRES_DB/${POSTGRES_DB}/g" webapps/ROOT/WEB-INF/hibernate.cfg.xml
+sed -i "s/POSTGRES_USER/${POSTGRES_USER}/g" webapps/ROOT/WEB-INF/hibernate.cfg.xml
+sed -i "s/POSTGRES_PASSWORD/${POSTGRES_PASSWORD}/g" webapps/ROOT/WEB-INF/hibernate.cfg.xml
 
 #L. Dubost PR #2, add personalised distribution Id
-sed -i "s/<id>org.xwiki.enterprise:xwiki-enterprise-web/<id>org.xwiki.enterprise:xwiki-enterprise-docker-zwindler/" webapps/xwiki/META-INF/extension.xed
+sed -i "s/<id>org.xwiki.enterprise:xwiki-enterprise-web/<id>org.xwiki.enterprise:xwiki-enterprise-docker-zwindler/" webapps/ROOT/META-INF/extension.xed
 
 bin/catalina.sh run
